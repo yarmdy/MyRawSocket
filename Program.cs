6259112460d021a3e59d216d2a291ebf22e35115
yarmdy <jxy2000121@sina.com>
2024-06-21 09:49:44 +0800

    修改问题

--- Program.cs
+++ Program.cs
@@ -4,11 +4,55 @@ using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+void toLittle<T>(byte[] data,int offset) where T:struct
+{ 
+    var tSize = Marshal.SizeOf(typeof(T));
+    var loc = offset;
+    if (data.Length < offset + tSize)
+    {
+        return;
+    }
+    foreach(var field in typeof(T).GetFields())
+    {
+        var fSize = Marshal.SizeOf(field.FieldType);
+        var i = loc;
+        loc += fSize;
+        if (fSize < 2 || fSize%2>0)
+        {
+            continue;
+        }
+        
 
-var socket = new Socket(AddressFamily.InterNetwork,SocketType.Raw,ProtocolType.IP);
+        var lf = 0;
+        do
+        {
+            var tmp = data[i + lf];
+            data[i + lf] = data[i+fSize-1-lf];
+            data[i + fSize - 1 - lf] = tmp;
+        } while (++lf < fSize / 2);
+    }
+}
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
 //socket.Blocking = false;
-socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.11"),0));
+var ips = Dns.GetHostAddresses(Dns.GetHostName());
+Console.WriteLine("请选择以下IP地址，并输入其中的序号：");
+var index = 0;
+ips.ToList().ForEach(ip =>Console.WriteLine($"{++index}: {ip}"));
+while (true)
+{
+    var key = Console.ReadKey();
+    if(!int.TryParse(key.KeyChar.ToString(),out index))
+    {
+        continue;
+    }
+    if (index > ips.Length || index < 1) {  
+        continue; 
+    }
+    break;
+}
+var myip = ips[index - 1];
+socket.Bind(new IPEndPoint(myip, 0));
 
 socket.SetSocketOption(SocketOptionLevel.IP,SocketOptionName.HeaderIncluded,true);
 
@@ -20,36 +64,44 @@ socket.IOControl(IOControlCode.ReceiveAll, optionIn, optionOut);
 
 
 byte[] buffer = new byte[102400];
+var sizeTcpHeader = Marshal.SizeOf<TcpHeader>();
+var sizeIPHeader = Marshal.SizeOf<IPHeader>();
 while (true)
 {
     try
     {
-        int bytesRead = await socket.ReceiveAsync(buffer);
+        int bytesRead = socket.Receive(buffer);
         var data = buffer.Take(bytesRead).ToArray();
 #pragma warning disable CS4014 // 由于此调用不会等待，因此在调用完成前将继续执行当前方法
         Task.Run(() => {
-            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf<IPHeader>());
-            Marshal.Copy(data, 0, ptr, Marshal.SizeOf<IPHeader>());
+            toLittle<IPHeader>(data, 0);
+            toLittle<TcpHeader>(data, sizeIPHeader);
+
+            var ptr = Marshal.AllocHGlobal(sizeIPHeader);
+            Marshal.Copy(data, 0, ptr, sizeIPHeader);
             IPHeader header = Marshal.PtrToStructure<IPHeader>(ptr);
             Marshal.FreeHGlobal(ptr);
-            var sIP = IPAddress.Parse(string.Join(".", BitConverter.GetBytes(header.SourceAddress)));
-            var tIP = IPAddress.Parse(string.Join(".", BitConverter.GetBytes(header.DestinationAddress)));
+            var sIP = IPAddress.Parse(string.Join(".", BitConverter.GetBytes(header.SourceAddress).Reverse()));
+            var tIP = IPAddress.Parse(string.Join(".", BitConverter.GetBytes(header.DestinationAddress).Reverse()));
+
             var protocol = header.Protocol;
-            if (bytesRead > 0)
-            {
-                // 解析 ICMP 数据包
-                Console.WriteLine($"接收：({protocol}){sIP}->{tIP}【{bytesRead}】");
-            }
+            
+
             if (protocol != 6)
             {
-                return; ;
+                return;
             }
-            ptr = Marshal.AllocHGlobal(Marshal.SizeOf<TcpHeader>());
-            Marshal.Copy(data, (header.VersionAndHeaderLength&0x0F)*4, ptr, Marshal.SizeOf<TcpHeader>());
+            ptr = Marshal.AllocHGlobal(sizeTcpHeader);
+            Marshal.Copy(data, sizeIPHeader, ptr, sizeTcpHeader);
             var tcpheader = Marshal.PtrToStructure<TcpHeader>(ptr);
             Marshal.FreeHGlobal(ptr);
-            Console.WriteLine($"TCP：({tcpheader.SequenceNumber}){tcpheader.SourcePort}->{tcpheader.DestinationPort}【{bytesRead}】");
-        });
+            if (!tIP.Equals(myip))
+            {
+                return;
+            }
+            Console.WriteLine($"TCP：{sIP}:{tcpheader.SourcePort}->{tIP}:{tcpheader.DestinationPort}【{bytesRead}】");
+
+        }).Wait();
 
     }
     catch(Exception ex)
@@ -65,7 +117,7 @@ public struct TcpHeader
     public ushort DestinationPort;      // 目标端口号，2字节
     public uint SequenceNumber;         // 序列号，4字节
     public uint AcknowledgmentNumber;   // 确认号，4字节
-    public byte DataOffsetAndFlags;     // 数据偏移和标志位，1字节
+    public ushort DataOffsetAndFlags;     // 数据偏移和标志位，1字节
     public ushort WindowSize;           // 窗口大小，2字节
     public ushort Checksum;             // 校验和，2字节
     public ushort UrgentPointer;        // 紧急指针，2字节
@@ -85,3 +137,4 @@ public struct IPHeader
     public uint SourceAddress;            // 源IP地址
     public uint DestinationAddress;       // 目标IP地址
 }
+
